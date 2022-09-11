a497e18f47b66054be2ee63a4df27b8d2441fc51
sushant1901 <mishrasushant1901@gmail.com>
2022-09-11 17:56:41 +0530

    Stones on the Table

---
+++ src/Solution10.java
@@ -0,0 +1,21 @@
+import java.util.Scanner;
+
+public class Solution10 {
+    public static void main(String[] args) {
+        Scanner sc= new Scanner(System.in);
+        int n=sc.nextInt();
+        String s=sc.next();
+        int count=0;
+        char z= s.charAt(0);
+        for(int i=1;i<s.length();i++){
+            if(z==s.charAt(i)){
+                count++;
+
+            }
+            z= s.charAt(i);
+        }
+
+        System.out.println(count);
+        sc.close();
+    }
+}
