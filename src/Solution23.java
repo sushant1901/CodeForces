0b3d1f22b8c8278bc256443a26f416be842967a4
sushant1901 <mishrasushant1901@gmail.com>
2022-09-22 12:43:33 +0530

    Presents

---
+++ src/Solution23.java
@@ -0,0 +1,29 @@
+import java.util.*;
+import java.io.*;
+public class Solution23
+{
+    public static void main(String args[])throws IOException
+    {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int n = Integer.parseInt(br.readLine());
+        String str = br.readLine();
+        String split[] = str.split(" ");
+        int a[] = new int[n];
+        for(int i=0;i<n;i++)
+            a[i] = Integer.parseInt(split[i]);
+        calculatePresents(a);
+    }
+
+    private static void calculatePresents(int a[])
+    {
+        int b[] = new int[a.length];
+        for(int i=0;i<a.length;i++)
+        {
+            int no = a[i];
+            b[no-1] = i;
+        }
+
+        for(int i=0;i<b.length;i++)
+            System.out.print(b[i]+1+" ");
+    }
+}
