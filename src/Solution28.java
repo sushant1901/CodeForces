dc420304eeee9de68f6b1eb09d28328aa069ee4a
sushant1901 <mishrasushant1901@gmail.com>
2022-09-27 18:50:10 +0530

    Hulk

--- src/Solution28.java
+++ src/Solution28.java
@@ -1,31 +1,15 @@
 import java.util.Scanner;
 public class Solution28{
-
     public static void main(String[] args) {
 
         Scanner myscanner = new Scanner(System.in);
-
         int feeling = myscanner.nextInt();
-
         for(int i=1; i<=feeling; i++)
         {
-            if(i%2 ==0)
-            {
-                System.out.print("I love ");
-            }
-            else
-            {
-                System.out.print("I hate ");
-            }
-
-            if(i == feeling)
-            {
-                System.out.print("it");
-            }
-            else
-            {
-                System.out.print("that ");
-            }
+            if(i%2 ==0) System.out.print("I love ");
+            else System.out.print("I hate ");
+            if(i == feeling) System.out.print("it");
+            else System.out.print("that ");
         }
     }
 }
