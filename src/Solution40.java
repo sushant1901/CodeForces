cb9e0efe3f6c59f8a80767849ac9ffe9ec175717
sushant1901 <mishrasushant1901@gmail.com>
2022-10-16 22:13:07 +0530

    Fox and Snake

---
+++ src/Solution40.java
@@ -0,0 +1,34 @@
+import java.util.Scanner;
+public class Solution40 {
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+        int n = scan.nextInt();
+        int m = scan.nextInt();
+
+        int mark=0;
+
+        for(int i=0;i<n;i++){
+            for(int j=0;j<m;j++){
+                if(i%2==0)
+                    System.out.print("#");
+                else if(i%2==1 && mark==0){
+                    if(j<m-1)
+                        System.out.print(".");
+                    else{
+                        mark =1;
+                        System.out.print("#");
+                    }
+                }
+                else if(i%2==1 && mark==1){
+                    if(j==0)
+                        System.out.print("#");
+                    else
+                        System.out.print(".");
+                    if(j==m-1)
+                        mark=0;
+                }
+            }
+            System.out.println("");
+        }
+    }
+}
